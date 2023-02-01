e038722a70cdafdb5fe6429011026dce36f1e3de
Ankit Mishra <ankitm8421@gmail.com>
2023-02-02 01:37:50 +0530

    first commit

---
+++ donn/index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>brain Mentors</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playball&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="logo"><h1 class="animate__animated animate__heartBeat animate__infinite	infinite " style="font-size: 30px;">Brain Mentors</h1></div>
+            <div class="menu">
+                <a href="index.html" class=button >Home</a>
+                <a href="select.html"class=button>Books</a>
+                <a href="#about"class=button>About</a>
+                <a href="login.php"class=button >Login</a>
+
+            </div>
+        </nav>
+            <main class="main">
+                <section>
+                    <h3>"Welcome to your Mentors World"</h3>
+                    <div class="wrapper">
+                      
+                      <ul class="dynamic-txts">
+                        <li><span>JEE-MAINS</span></li>
+                        <li><span>NEET-UGC</span></li>
+                        <li><span>JEE-ADVANCED</span></li>
+                        <li><span>UPSC</span></li>
+                        <li><span>NTA</span></li>
+                        <li><span>JAM</span></li>
+                      </ul>
+                    </div>
+                    <h1>We Provide    <span class="change_content"></span></h1>
+                    <a href="login.php" class="button1">   Quiz   </a>
+                    <a href="cources.html" class="button2">     Courses     </a>
+
+                </section>
+                <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+                <lottie-player src="https://assets6.lottiefiles.com/packages/lf20_oojuetow.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
+                
+            </main>
+            <div class="text1"><h5 class="animate__animated animate__flipInX animate__infinite	infinite ">Academic Session</h5></div>
+    </header>
+    
+    <section>
+      <div class=abhi>
+        
+        <article>
+          <div class="cat">
+            <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+            <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_zxadsdst.json" 
+            background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
+            <a href="select4.html"><button class="btn">Recorded lecture</button></a>
+
+
+          </div>
+
+        </article>
+        <article><div class="cat">
+          <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+          <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_ketv1eqz.json"
+          background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop autoplay></lottie-player>
+          <a href="login.php" ><button class="btn">Mock T
+            
+          
+          
+          est</button></a>
+        </div></article>
+
+        
+          <article>
+            <div class="cat">
+              <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+              <lottie-player src="https://assets3.lottiefiles.com/packages/lf20_DMgKk1.json" mode="bounce" background="transparent"  speed="2"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
+              <a href="select.html" ><button class="btn">Library</button></a>
+  
+  
+            </div>
+  
+          </article>
+
+          <article>
+            <div class="cat">
+              <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+              <lottie-player src="https://assets6.lottiefiles.com/packages/lf20_oojuetow.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
+              <a href="https://www.doubtnut.com/"><button class="btn">Ask Doubt</button></a>
+  
+  
+            </div>
+  
+          </article>
+          
+
+      
+    </section>
+
+
+    <section>
+      <div class="text12"><h5 class="animate__animated animate__flipInX animate__infinite	infinite ">Our Team</h5></div>
+      
+    
+      <div class="abhishek">
+          
+        <main class="grid">
+              
+          <article>
+                    
+            <div class="text">
+              <div>
+                <img  class="avtar"src="img\bbb.jpeg">
+                  </div>
+                    <h3>SUNIL</h3>
+                      <p class="pc">"Overall a very informative content which delivered in Brain Mentors. Course 
+                        content got well covered and also demonstrated the concept very well. Thanks for such an informative and concept-clearing 
+                        training session"
+                      </p>
+                    <a href="https://www.linkedin.com/in/sunil-gupta-bba094223"><button>Contact</button></a>
+    
+            </div>
+          </article>
+          <article>
+                    
+            <div class="text">
+              <div>
+                <img  class="avtar"src="img\praj.jpeg">
+                  </div>
+                      <h3>PRAJWAL</h3>
+                      
+                        
+                        <p class="pc">"It was excellent journey with Brain Mentors . Among all batches also got come new daily using tricks as well as understand all lecture 
+                          wonderful and Thanks for sharing recording for future study"
+
+                        </p>
+                    <a href="https://www.linkedin.com/in/atul-pal-457292208"><button >contact</button></a>
+    
+            </div>
+          </article>
+          <article>
+                    
+            <div class="text">
+              <div>
+                <img  class="avtar"src="img\ankit.jpg">
+                  </div>
+                    <h3>ANKIT</h3>
+                      <p class="pc">"Very supportive staff and their knowledge is god level. Very easy to understand & the video provided
+                          after the every sessions is very helpful. Completely based on practical knowledge which makes it easy to understand"
+                      </p>
+                      <a href="https://www.linkedin.com/in/iamankitm"><button >contact</button></a>
+            </div>
+            
+          </article>
+          <article>
+                    
+            <div class="text">
+              <div>
+                <img  class="avtar"src="img\abhishek.jpg">
+                  </div>
+                    <h3>ABHISHEK</h3>
+                      <p class="pc">"Overall a very informative content which delivered in Brain Mentors. Course 
+                        content got well covered and also demonstrated the concept very well. Thanks for such an informative and concept-clearing 
+                        training session"
+                      </p>
+                    <a href="https://instagram.com/mr.proton2?igshid=YmMyMTA2M2Y="><button>Contact</button></a>
+    
+            </div>
+          </article>
+        </main>
+        
+      
+      </div>
+
+
+    </section>
+
+    
+    <!-- <class class="counter">
+    <div id="sfcs24bf321t11paksy9r4w9mkn9l26ywzr"></div>
+<script type="text/javascript" src="https://counter7.stat.ovh/private/counter.js?c=s24bf321t11paksy9r4w9mkn9l26ywzr&down=async" async></script>
+<noscript><a href="https://www.freecounterstat.com" title="website counter"><img src="https://counter7.stat.ovh/private/freecounterstat
+  .php?c=s24bf321t11paksy9r4w9mkn9l26ywzr" border="0" title="website counter" alt="website counter"></a></noscript>
+  </class> -->
+  <class class="counter">
+  <div id="sfcs24bf321t11paksy9r4w9mkn9l26ywzr"></div>
+  <script type="text/javascript" src="https://counter9.stat.ovh/private/counter.js?c=w1a65n1h3cu8j7ukka7j4ffjxmp7wdwq&down=async" async></script>
+  <noscript><a href="https://www.freecounterstat.com" title="free hit counter"><img src="https://counter9.stat.ovh/private/freecounterstat
+  .php?c=w1a65n1h3cu8j7ukka7j4ffjxmp7wdwq" border="0" title="free hit counter" alt="free hit counter"></a></noscript>
+</class>
+
+
+
+
+  <hr>
+    <footer id="about">
+        <div class="content">
+          <div class="left box">
+            <div class="upper">
+              <div class="topic">About us</div>
+              <p>Brain mentors is a place where you can shap your mind with the help of our mentors</p>
+            </div>
+            <div class="lower">
+              <div class="topic">Contact us</div>
+              <div class="phone">
+                <a href="#"><i class="fas fa-phone-volume"></i>8779190379</a>
+              </div>
+              <div class="email">
+                <a href="#"><i class="fas fa-envelope"></i>BrainMentors@gmail.com</a>
+              </div>
+            </div>
+          </div>
+          <div class="middle box">
+            <div class="topic">Our Services</div>
+            <div><a href="#">Our Exclusive staff</a></div>
+            <div><a href="#">Signup</a></div>
+            <div><a href="#">Explore our another branches</a></div>
+          </div>
+          <div class="right box">
+            <div class="topic">Subscribe us</div>
+            <form action="#">
+              <input type="text" placeholder="Enter email address">
+              <input type="submit" name="" value="Send">
+              <div class="media-icons">
+                <a href="https://www.facebook.com/Chemestry-Pathsala-111913571172406"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://www.instagram.com/ysabhishekmishra/?utm_medium=copy_link"><i class="fab fa-instagram"></i></a>
+                <a href="https://twitter.com/Ysabhishekmish2"><i class="fab fa-twitter"></i></a>
+                <a href=""><i class="fab fa-youtube"></i></a>
+                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="bottom">
+          <p>Copyright © 2022 <a href="#">Brain Mentors</a> All rights reserved</p>
+        </div>
+      </footer> 
+    
+</body>
+</html>
